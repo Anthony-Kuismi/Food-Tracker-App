88c6d5ac0ed3748cdbec0b9dd18d35a1a2888662
Samuel Ortiz Fishman <ortiz466@d.umn.edu>
2024-03-13 21:30:34 -0500

    created custum navbar file

---
+++ lib/viewmodel/customNavBar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class customNavBar extends StatelessWidget{
+  const customNavBar ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+}
