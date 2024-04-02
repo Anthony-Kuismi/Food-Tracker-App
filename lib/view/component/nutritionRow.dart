13665f58592da658b6231696754de664d352bbcf
Anthony-Kuismi <kuism006@d.umn.edu>
2024-04-02 10:05:20 -0500

    added editing ui but it does not svae the value yet.

--- lib/view/component/nutritionRow.dart
+++ lib/view/component/nutritionRow.dart
@@ -1,20 +1,66 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import '../../model/food.dart';
 import '../food_view.dart';
 
-class NutritionRow extends Row{
-  NutritionRow(label, value, measurement, {super.key}) : super(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text('Protein: $value $measurement'),
-        Padding(
-            padding: const EdgeInsets.fromLTRB(0,0,0,0),
-            child: IconButton(
-              onPressed: (){
-              },
-              icon:  const Icon(Icons.edit, size: 18),
-            )
-        ),
-      ]
-  );
+class NutritionRow extends StatefulWidget{
+  final String label;
+  var value;
+  final String measurement;
+
+  NutritionRow(this.label, this.value, this.measurement, {super.key});
+
+  @override
+  NutritionRowState createState() => NutritionRowState();
+}
+
+class NutritionRowState extends State<NutritionRow>{
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        showDialog(
+            context: context, 
+            builder: (BuildContext context){
+              TextEditingController controller = TextEditingController();
+              return AlertDialog(
+                title: Text('Editing ${widget.label}'),
+                content: TextField(
+                  controller: controller,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter New Value',
+                  ),
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      double? newValue = double.tryParse(controller.text);
+                      if (newValue != null) {
+                        widget.value = newValue;
+                      }
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('OK'),
+                  ),
+                ],
+              );
+            }
+        );
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('${widget.label}${widget.value}${widget.measurement}'),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(6,4,0,0),
+            child: Icon(
+              Icons.edit
+            ),
+          ),
+        ]
+      ),
+    );
+  }
 }
