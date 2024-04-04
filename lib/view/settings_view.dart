195cd6189c34d7ac6f48a0a1d485d888f2999237
malec230 <malec230@d.umn.edu>
2024-04-04 14:46:25 -0500

    settings profile ui/ux

--- lib/view/settings_view.dart
+++ lib/view/settings_view.dart
@@ -3,7 +3,6 @@ import 'package:provider/provider.dart';
 import '../Service/auth_service.dart';
 import 'component/navbar.dart';
 
-
 class SettingsView extends StatelessWidget {
   const SettingsView({super.key});
 
@@ -20,16 +19,102 @@ class SettingsView extends StatelessWidget {
           ),
         ),
       ),
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: const Text('Logout'),
-            leading: const Icon(Icons.logout),
-            onTap: () {
-              Provider.of<AuthService>(context, listen: false).logout(context);
-            },
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.black26,
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        'User Name',
+                        style: TextStyle(
+                          fontSize: Theme.of(context).textTheme.titleLarge?.fontSize,
+                          color: Theme.of(context).colorScheme.primary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Theme.of(context).colorScheme.primaryContainer,
+                            ),
+                            child: Text(
+                              'Weight: 160lbs',
+                              style: TextStyle(
+                                fontSize: Theme.of(context).textTheme.titleSmall?.fontSize,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Theme.of(context).colorScheme.primaryContainer,
+                            ),
+                            child: Text(
+                              'Height: 5\"9\'', // Replace with actual height
+                              style: TextStyle(
+                                fontSize: Theme.of(context).textTheme.titleSmall?.fontSize,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Theme.of(context).colorScheme.primaryContainer,
+                            ),
+                            child: Text(
+                              'Gender: Male', // Replace with actual gender
+                              style: TextStyle(
+                                fontSize: Theme.of(context).textTheme.titleSmall?.fontSize,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.black26,
+                ),
+                child: ListTile(
+                  title: const Text('Logout'),
+                  leading: const Icon(Icons.logout),
+                  onTap: () {
+                    Provider.of<AuthService>(context, listen: false).logout(context);
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: const NavBar(key: Key('customNavBar')),
     );
